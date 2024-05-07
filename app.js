a85ae1b6f9395e663feeb4de8d530398d0d5fe03
kshitijasaswade <61146709+kshitijasaswade123@users.noreply.github.com>
2024-05-07 22:35:35 +0530

    port change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const ShoppingRoute = require('./Route/ShoppingRoute');
-const PORT = process.env.port;
+const PORT = process.env.port || 5000;
 
 app.get('/', (req, res) => {
   res.send("hellloo");
